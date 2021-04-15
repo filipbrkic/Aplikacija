233a9134630a4d983128e64a52f82f37b70ddf95
Filip Brkić <filipbrkic5@gmail.com>
2021-04-15 11:21:16 +0200

    popravljanje prebacivanja iz date u model

--- Aplikacija/Models/Predbiljezba.cs
+++ Aplikacija/Models/Predbiljezba.cs
@@ -4,7 +4,7 @@ using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace Aplikacija.Data
+namespace Aplikacija.Models
 {
     public class Predbiljezba
     {
