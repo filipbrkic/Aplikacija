caedac01ace35d088abb854c68c8ec95deb76c12
Filip Brkić <filipbrkic5@gmail.com>
2021-04-15 11:24:09 +0200

    3. dio popravljanja

--- Aplikacija/Data/AplikacijaContext.cs
+++ Aplikacija/Data/AplikacijaContext.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace Aplikacija.Data
+namespace Aplikacija.Models
 {
     public class AplikacijaContext : DbContext
     {
