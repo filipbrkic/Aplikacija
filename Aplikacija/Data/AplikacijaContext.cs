fb6db34e32217ead3e5a6f31c444601416064cdc
Filip Brkić <filipbrkic5@gmail.com>
2021-04-16 12:18:03 +0200

    modelbuilder

--- Aplikacija/Data/AplikacijaContext.cs
+++ Aplikacija/Data/AplikacijaContext.cs
@@ -1,10 +1,11 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Aplikacija.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace Aplikacija.Models
+namespace Aplikacija.Data
 {
     public class AplikacijaContext : DbContext
     {
@@ -15,5 +16,12 @@ namespace Aplikacija.Models
         public DbSet<Predbiljezba> Predbiljezbas { get; set; }
         public DbSet<Seminar> Seminars { get; set; }
         public DbSet<Zaposlenik> Zaposleniks { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Predbiljezba>().ToTable("Predbiljezba");
+            modelBuilder.Entity<Seminar>().ToTable("Seminar");
+            modelBuilder.Entity<Zaposlenik>().ToTable("Zaposlenik");
+        }
     }
 }
