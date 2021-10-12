62259a5ad573ff2f895494db053fa89f50f1a0f8
Filip Brkić <filipbrkic5@gmail.com>
2021-10-12 10:37:01 +0200

    fixes

--- Application.MVC/Models/RegistrationViewModel.cs
+++ Application.MVC/Models/RegistrationViewModel.cs
@@ -7,19 +7,27 @@ namespace Application.MVC.Models
     {
         public Guid Id { get; set; }
 
+        [Required]
         [DataType(DataType.Date)]
         [Display(Name = "Date of birth")]
         public DateTime DateTime { get; set; }
 
+        [Required]
         [Display(Name = "First Name")]
         public string FirstName { get; set; }
 
+        [Required]
         [Display(Name = "Last Name")]
         public string LastName { get; set; }
+
+        [Required]
         public string Address { get; set; }
 
+        [Required]
         [EmailAddress(ErrorMessage = "Invalid Email Adress")]
         public string Email { get; set; }
+
+        [Required]
         public string Phone { get; set; }
         public Guid SeminarId { get; set; }
         public bool Status { get; set; }
